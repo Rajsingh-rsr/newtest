50bf377757e37b68ec796fdf638e8b2a63a1dd4e
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-03-19 01:15:22 +0545

    remove form middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -14,7 +14,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(upload.none(), registerUser);
+router.route("/register").post(registerUser);
 router.route("/login").post(loginUser)
 
 // secured routers
